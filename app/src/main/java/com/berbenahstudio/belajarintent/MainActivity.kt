babde7ba7010906dd4d859e94b571c3982418b20
rizkitriandani <rizkitriandani@yahoo.co.id>
2021-08-07 00:39:44 +0700

    Added UI element and testing onClickListener Event.

--- app/src/main/java/com/berbenahstudio/belajarintent/MainActivity.kt
+++ app/src/main/java/com/berbenahstudio/belajarintent/MainActivity.kt
@@ -2,10 +2,25 @@ package com.berbenahstudio.belajarintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnIntentExplicit: Button = findViewById(R.id.btnIntentExplicit)
+
+        btnIntentExplicit.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btnIntentExplicit ->{
+                Toast.makeText(this, "ANDA MENEKAN TOMBOL ${R.string.intent_explicit}",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
